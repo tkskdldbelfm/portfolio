9ccca5238c6f841759560350e4173958f847722f
tkskdldnelfm <kakd1313@naver.com>
2023-11-12 18:47:34 +0900

    update

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+// 페이지가 로드될 때 실행
+window.addEventListener("load", () => {
+    // 헤더 엘리먼트 가져오기
+    const header = document.querySelector("nav");
+
+    // 이전 스크롤 위치 저장
+    let prevScrollPos = window.pageYOffset;
+
+    // 스크롤 이벤트 리스너 등록
+    window.addEventListener("scroll", () => {
+        // 현재 스크롤 위치 가져오기
+        const currentScrollPos = window.pageYOffset;
+
+        // 스크롤 방향 계산
+        if (prevScrollPos > currentScrollPos) {
+            // 스크롤을 올릴 때: 헤더 표시
+            header.style.opacity = "1";
+        } else {
+            // 스크롤을 내릴 때: 헤더 숨김
+            header.style.opacity = "0";
+        }
+
+        // 현재 스크롤 위치를 이전 스크롤 위치로 업데이트
+        prevScrollPos = currentScrollPos;
+    });
+
+});
+
+
+
+
 var polyline = document.querySelector('.drawing_line_polyline');
 var polyPoints = polyline.getAttribute('points');
 var circle = document.querySelector('.drawing_line_circle');
