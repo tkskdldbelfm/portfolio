dd356a8d72d4a56e4653be0ba50637b420f214db
tkskdldnelfm <kakd1313@naver.com>
2023-12-27 17:11:21 +0900

    sdad

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const mysql = require('mysql');
 const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
 
 
 const app = express();
@@ -21,15 +22,7 @@ const dbConfig = {
 // MariaDB 연결
 const pool = mysql.createPool(dbConfig);
 
-const connection = mysql.createConnection(dbConfig);
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Database connection error:', err);
-        return;
-    }
-    console.log('Connected to the database');
-});
 
 // 데이터베이스에 접속 기록을 저장하기 위한 테이블 생성
 const createVisitLogsTable = `
@@ -55,11 +48,15 @@ app.use(session({
     secret: process.env.SECRET_KEY, // 쿠키에 서명을 추가하여 보안을 강화합니다.
     resave: false,
     saveUninitialized: true,
+    store: sessionStore,
     cookie: {
         secure: true,// HTTPS를 사용하는 경우 true로 변경
         maxAge: 60 * 60 * 1000, // 세션의 유효 시간을 1시간으로 설정 (밀리초 단위) 
     }
 }));
+
+const sessionStore = new MySQLStore(dbConfig, mysql.createConnection(dbConfig));
+
 
 // 정적 파일을 제공하기 위해 express.static 미들웨어를 사용합니다.
 app.use(express.static(path.join(__dirname, 'public')));
@@ -78,7 +75,10 @@ app.use('/', indexRoute);
 // 루트 경로에 대한 라우트
 app.get('/', (req, res) => {
     // 클라이언트의 세션에 방문 여부를 확인합니다.
-    if (!req.session.visited) {
+    if (!req.session.visited || req.session.visited === undefined) {
+        // 처음 방문한 경우에만 visited 속성을 설정합니다.
+        req.session.visited = true;
+
         // 방문자 수를 1 증가시킵니다.
         pool.getConnection((err, connection) => {
             if (err) {
@@ -86,7 +86,7 @@ app.get('/', (req, res) => {
                 return res.status(500).send('Internal Server Error');
             }
 
-            const updateVisitorCountQuery = 'UPDATE visitors SET count = count + 1 WHERE id = ?';
+            const updateVisitorCountQuery = 'UPDATE visit_logs SET count = count + 1 WHERE id = ?';
             connection.query(updateVisitorCountQuery, [req.sessionID], (err) => {
                 if (err) {
                     connection.release();
@@ -94,11 +94,8 @@ app.get('/', (req, res) => {
                     return res.status(500).send('Internal Server Error');
                 }
 
-                // 클라이언트의 세션에 방문 여부를 기록합니다.
-                req.session.visited = true;
-
                 // 방문자 수를 조회합니다.
-                const selectVisitorCountQuery = 'SELECT count FROM visitors WHERE id = ?';
+                const selectVisitorCountQuery = 'SELECT count FROM visit_logs WHERE visitor_id = ?';
                 connection.query(selectVisitorCountQuery, [req.sessionID], (err, results) => {
                     if (err) {
                         connection.release();
@@ -133,7 +130,7 @@ app.get('/', (req, res) => {
                 return res.status(500).send('Internal Server Error');
             }
 
-            const selectVisitorCountQuery = 'SELECT count FROM visitors WHERE id = ?';
+            const selectVisitorCountQuery = 'SELECT count FROM visit_logs WHERE visitor_id = ?';
             connection.query(selectVisitorCountQuery, [req.sessionID], (err, results) => {
                 if (err) {
                     connection.release();
