f790fd4b3b8885c3e6320bc440fad36cf4d4774c
tkskdldnelfm <kakd1313@naver.com>
2023-12-27 20:45:55 +0900

    asd

--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,37 @@
 // routes/index.js
 const express = require('express');
 const router = express.Router();
-const mysql = require('mysql');
-const path = require('path');
+
 const pool = require('../app').pool; // app.js에서 가져옴
 
 
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
     if (!req.session.visited) {
         // 방문자 수 증가 및 방문 기록
-        try {
-            const connection = await pool.getConnection();
-            await connection.query('INSERT INTO visitor_log (timestamp) VALUES (NOW())');
-            connection.release();
+        pool.getConnection((err, connection) => {
+            if (err) {
+                console.error('Error connecting to MariaDB:', err);
+                return res.status(500).send('Internal Server Error');
+            }
 
-            // 세션에 방문 여부 표시
-            req.session.visited = true;
-        } catch (error) {
-            console.error('Error while updating visitor count:', error);
-        }
+            connection.query('INSERT INTO visitor_log (timestamp) VALUES (NOW())', (error) => {
+                connection.release();
+
+                if (error) {
+                    console.error('Error while updating visitor count:', error);
+                    return res.status(500).send('Internal Server Error');
+                }
+
+                // 세션에 방문 여부 표시
+                req.session.visited = true;
+
+                res.render('index'); // 또는 res.send('Welcome to the website!');
+            });
+        });
+    } else {
+        res.render('index'); // 또는 res.send('Welcome to the website!');
     }
-
-    res.render('index'); // 또는 res.send('Welcome to the website!');
 });
 
 
